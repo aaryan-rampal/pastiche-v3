eb5784dca36f2fdba76459cba5e4539847db23c2
Aaryan Rampal <76913929+aaryan-rampal@users.noreply.github.com>
2025-09-24 00:50:45 -0700

    Add script to clean and sample classes data
    - Introduced `clean_classes.py` to process the classes dataset.
    - Checked for the existence of files in a specified directory.
    - Filtered and sampled 4000 rows where files exist.
    - Ensured artist and genre names are properly capitalized.
    - Exported the cleaned and sampled data to `classes_truncated.csv`.

---
+++ data/clean_classes.py
@@ -0,0 +1,39 @@
+# %%
+import os
+import pandas as pd
+# %%
+DATA_DIR = "/Volumes/Extreme SSD/wikiart"
+# %%
+df = pd.read_csv("classes.csv")
+
+# %%
+exists = []
+for row in df.itertuples():
+    filename = row.filename
+    full_path = os.path.join(DATA_DIR, filename)
+    exists.append(os.path.exists(full_path))
+df["exists"] = exists
+
+# %%
+# sample 4000 rows where exists is True
+df_filtered = df[df["exists"] == True]
+df_sampled = df_filtered.sample(n=4000, random_state=42)
+print("Sampled number of rows:", len(df_sampled))
+
+# %%
+# checking unique phash in sampled data
+print("Unique phash in sampled data:", df_sampled["phash"].nunique())
+print("Sampled number of rows:", len(df_sampled))
+# %%
+# %%
+# make start of artist names uppercase
+df_sampled["artist"] = df_sampled["artist"].str.title()
+df_sampled["artist"].value_counts()
+# %%
+df_sampled["genre"] = df_sampled["genre"].str.title()
+df_sampled["genre"].value_counts()
+
+# %%
+df_sampled.to_csv("classes_truncated.csv", index=False)
+
+# %%
